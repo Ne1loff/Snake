afa571d588ed7cb9e57b059507d9b17a16fa5b02
sokket <sokket@oceancraft.ru>
2020-06-28 12:54:49 +0300

    Fix record file creation and path

--- src/main/java/com/sergey/snake/MainWindow.java
+++ src/main/java/com/sergey/snake/MainWindow.java
@@ -3,20 +3,19 @@ package com.sergey.snake;
 import javax.swing.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class MainWindow extends JFrame {
     public static final int width = 640;
     public static final int height = width + 41;
-    public MainWindow () {
-        File file = new File("C:\\Users\\serge\\IdeaProjects\\Snake\\build\\libs");
-        int counter = 0;
-        for (File findRec : Objects.requireNonNull(file.listFiles())) {
-            if (findRec.getName().equals("record.txt")) {
-                counter = 1;
-            }
-        }
-        if (counter == 0) {
+
+    public MainWindow() {
+        Path recordFilePath = Paths.get("record.txt");
+
+        if (!Files.exists(recordFilePath)) {
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                     new FileOutputStream("record.txt"), StandardCharsets.UTF_8))) {
                 writer.write('0');
@@ -24,6 +23,7 @@ public class MainWindow extends JFrame {
                 e.printStackTrace();
             }
         }
+
         setTitle("Змейка");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(width, height);
